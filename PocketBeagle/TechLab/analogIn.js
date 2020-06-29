f099e1d3f068835b197ce1d26d44560087206729
Darren Schachter <dts86@cornell.edu>
2020-06-29 14:14:32 -0400

    Add pin variable for logging purposes

--- PocketBeagle/TechLab/analogIn.js
+++ PocketBeagle/TechLab/analogIn.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 var fs = require('fs');
+var pin = "P1_19"
 var AIN0 = '/sys/bus/iio/devices/iio:device0/in_voltage0_raw'; // Pin: P1_19
 
 var maxValue = 4095;  // maximum value for analog read
@@ -12,7 +13,7 @@ function printStatus(err, x) {
 
   var value = parseInt(x, 10) / maxValue;
 
-  process.stdout.write('P1_19: ' + (value*100).toFixed(1) +
+  process.stdout.write(pin + ": " + (value*100).toFixed(1) +
     '%, ' + (1.8*value).toFixed(3) + 'V   \r');
   setTimeout(doAnalogRead, 100);
 }
