6e97691e13e64ed07d7862f8234a087ce4f297dd
Deepak Khatri <deepaklorkhatri7@gmail.com>
2020-08-04 23:43:25 +0530

    add & update macros for BBB and BBAI PRU pins

--- common/prugpio.h
+++ common/prugpio.h
@@ -21,25 +21,73 @@
 #define USR4 (1<< 7)
 // GPIO5
 #define USR1 (1<< 5)
-// The define a couple of GPIO pin addresses
-// GPIO6
-#define P9_25   (1<<17)
-// GPIO8
-#define P8_17   (1<18)
-
-// These define which bin in register R30 go to which header pins
-// R30 bits - Output on pru1_1
-#define P8_15	(1<<16)
-#define P8_16	(1<<18)
-#define P8_26	(1<<17)
-// #define P9_14	(1<<14)     // Not defaulted to PRU
-#define P9_16	(1<<15)
-// R31 bits - Input on pru1_1
-#define P8_13	(1<<7)
-#define P8_18	(1<<5)
-#define P8_19	(1<<6)
-#define P9_19	(1<<2)
-#define P9_20	(1<<1)
+// PRU header pins
+// pru1_0
+#define P8_11   (1<<4)      /* pr1_pru0 */
+#define P8_12   (1<<3)      /* pr1_pru0 */
+#define P9_15   (1<<5)  	/* pr1_pru0 */
+#define P9_26   (1<<17)		/* pr1_pru0 */
+// pru1_1
+#define P8_13   (1<<7)      /* pr1_pru1 */
+#define P8_14   (1<<9)      /* pr1_pru1 */
+#define P8_15   (1<<16)     /* pr1_pru1 */
+#define P8_16   (1<<18)     /* pr1_pru1 */
+#define P8_18   (1<<5)		/* pr1_pru1 */
+#define P8_19   (1<<6)		/* pr1_pru1 */
+#define P8_26   (1<<17)		/* pr1_pru1 */
+#define P9_14   (1<<14)		/* pr1_pru1 */
+#define P9_16   (1<<15)		/* pr1_pru1 */
+#define P9_19   (1<<2)		/* pr1_pru1 */
+#define P9_20   (1<<1)		/* pr1_pru1 */
+#define P9_27   (1<<11)		/* pr1_pru1 */
+#define P9_41   (1<<3)		/* pr1_pru1 */
+#define P9_42   (1<<10)		/* pr1_pru1 */
+// pru2_0
+#define P8_03   (1<<10)     /* pr2_pru0 */
+#define P8_04   (1<<11)     /* pr2_pru0 */
+#define P8_05   (1<<6)      /* pr2_pru0 */
+#define P8_06   (1<<7)      /* pr2_pru0 */
+#define P8_08   (1<<20)     /* pr2_pru0 */
+#define P8_17   (1<<15)		/* pr2_pru0 */
+#define P8_20   (1<<3)		/* pr2_pru0 */
+#define P8_21   (1<<2)		/* pr2_pru0 */
+#define P8_22   (1<<9)		/* pr2_pru0 */
+#define P8_23   (1<<8)		/* pr2_pru0 */
+#define P8_24   (1<<5)		/* pr2_pru0 */
+#define P8_25   (1<<4)		/* pr2_pru0 */
+#define P8_28   (1<<17)		/* pr2_pru0 */
+#define P8_29   (1<<18)		/* pr2_pru0 */
+#define P8_30   (1<<19)		/* pr2_pru0 */
+#define P8_31   (1<<11)		/* pr2_pru0 */
+#define P8_33   (1<<10)		/* pr2_pru0 */
+#define P8_34   (1<<8)		/* pr2_pru0 */
+#define P8_35   (1<<9)		/* pr2_pru0 */
+#define P8_36   (1<<7)		/* pr2_pru0 */
+#define P8_37   (1<<5)		/* pr2_pru0 */
+#define P8_38   (1<<6)		/* pr2_pru0 */
+#define P8_39   (1<<3)		/* pr2_pru0 */
+#define P8_40   (1<<4)		/* pr2_pru0 */
+#define P8_41   (1<<1)		/* pr2_pru0 */
+#define P8_42   (1<<2)		/* pr2_pru0 */
+#define P8_44   (1<<0)		/* pr2_pru0 */
+#define P9_11   (1<<14)		/* pr2_pru0 */
+#define P9_13   (1<<15)		/* pr2_pru0 */
+// pru2_1
+#define P8_07   (1<<16)     /* pr2_pru1 */
+#define P8_09   (1<<6)      /* pr2_pru1 */
+#define P8_10   (1<<15)     /* pr2_pru1 */
+#define P8_27   (1<<17)		/* pr2_pru1 */
+#define P8_32   (1<<0)		/* pr2_pru1 */
+#define P8_43   (1<<20)		/* pr2_pru1 */
+#define P8_45   (1<<18)		/* pr2_pru1 */
+#define P8_46   (1<<19)		/* pr2_pru1 */
+#define P9_17   (1<<9)		/* pr2_pru1 */
+#define P9_18   (1<<8)		/* pr2_pru1 */
+#define P9_25   (1<<5)		/* pr2_pru1 */
+#define P9_28   (1<<13)		/* pr2_pru1 */
+#define P9_29   (1<<11)		/* pr2_pru1 */
+#define P9_30   (1<<12)		/* pr2_pru1 */
+#define P9_31   (1<<10)		/* pr2_pru1 */
 
 // Shared memory
 #define AM33XX_DATARAM0_PHYS_BASE1		0x4b200000
@@ -71,11 +119,13 @@
 #define P9_16 (1<<19)
 
 // The define a couple of GPIO pin addresses on Pocket
+// GPIO 0
+#define P2_05	(0x1<<30)
 // GPIO1
 #define P2_1 (1<<18)
 #define P1_32 (1<<10)
 
-// R30 output bits on pru0
+// R31/R30 input/output bits on pru0
 #define P9_31   (1<<0)
 #define P9_29   (1<<1)
 #define P9_30   (1<<2)
@@ -84,6 +134,16 @@
 #define P9_27   (1<<5)
 #define P9_91   (1<<6)
 #define P9_25   (1<<7)
+// R30 output bits on pru0
+#define P8_11   (1<<15) /* output */
+#define P8_12   (1<<14) /* output */
+// R31 input bits on pru0
+#define P8_15   (1<<15) /* input */
+#define P8_16   (1<<14) /* input */
+#define P9_24   (1<<16) /* input */
+
+// R31 input bits on pru1
+#define P9_26   (1<<16) /* input */
 
 // R30 output bits on pru0 on Pocket
 #define P1_36   (1<<0)
